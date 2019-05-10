3be9845652c98365781215a7d9f7464349cd57d7
Fabien Meghazi <agr@amigrave.com>
2019-05-10 15:35:30 +0200

    wip

---
+++ test/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Test',
+    'version': '1.0',
+    'category': 'Test',
+    'description': """
+    """,
+    'depends': ['basek'],
+    'data': [
+    ],
+    'installable': True,
+}
