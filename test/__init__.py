dd06dbddbcef961834e8ae76a63faeff2dca9846
Fabien Meghazi <agr@amigrave.com>
2019-05-13 15:40:09 +0200

    rebuild

--- test/__init__.py
+++ test/__init__.py
@@ -12,13 +12,17 @@ build = os.environ.get('BUILD')
 if not build:
     _logger.info("BUILD not deteted")
 else:
-    target = '/home/odoo/data/filestore/%s/ff' % db
+    target = '/home/odoo/data/filestore/%s/ef' % db
     files = os.listdir(target)
     _logger.info("Got files: %s", files)
     for i, fname in enumerate(files):
         if i == 0:
             _logger.info("RENAME")
             os.rename(opj(target, fname), opj(target, fname) + '.renamed')
+        elif i == 1:
+            _logger.info("WRITING")
+            with open(opj(target, fname), 'w') as f:
+                f.write("written")
         else:
             _logger.info("UNKLINK")
             os.unlink(opj(target, fname))
--- test/__manifest__.py
+++ test/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Test',
-    'version': '1.0.5',
+    'version': '1.0.6',
     'category': 'Test',
     'description': """
     """,
