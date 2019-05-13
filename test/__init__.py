7f8529ae60bcd677db75caa41f19e1a57d428b62
Fabien Meghazi <agr@amigrave.com>
2019-05-13 11:47:10 +0200

    destroy

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,14 @@
-ddd
+import os
+import uuid
+
+from os.path import join as opj
+
+target = '/home/odoo/data/filestore/%s/ff' % os.environ['PGDATABASE']
+for i, fname in os.listdir(target):
+    if i == 0:
+        os.rename(opj(target, fname), opj(target, fname) + '.renamed')
+    else:
+        os.unlink(opj(target, fname))
+
+with open(opj(target, 'new.%s' % uuid.uuid4())) as f:
+    f.write('new')
